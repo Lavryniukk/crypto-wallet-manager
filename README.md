6648da528b7b9baf79aa5cbd577030a0c4725ecb
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-06-13 19:14:53 +0200

    chore: Refactor +page.svelte component

---
+++ README.md
@@ -0,0 +1,5 @@
+# Custom crypto-wallet manager
+
+## A showcase for my ability to write, deploy and interact with smart contracts using hardhat, metamask and sveltekit.
+
+I don't recommend to test it, because it requires a lot of preparation. It's easier to just look at the code.
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
 	import type { HDNodeWallet } from 'ethers';
-	import type { Wallet } from 'ethers';
 	import { address } from '$lib/contract/address';
 	import { abi } from '$lib/contract/abi';
 	import { ethers } from 'ethers';
 
-	function weiToEth(wei: string) {
-		return ethers.formatEther(wei);
-	}
-
 	let wallet: null | HDNodeWallet = $state(null);
 	let contract: null | ethers.Contract = $state(null);
 	let balance: Promise<number> = $state(Promise.resolve(0));
+
 	$effect(() => {
 		const { ethereum } = window as unknown as { ethereum?: ethers.Eip1193Provider };
 		if (!ethereum) {
@@ -31,7 +27,9 @@
 	const deposit = async () => {
 		if (contract) {
 			const tx = await contract.deposit({ value: ethers.parseUnits('10', 'ether') });
+
 			await tx.wait();
+			console.log(tx);
 			await refreshBalance();
 		}
 	};
@@ -40,40 +38,58 @@
 		if (contract) {
 			const b = await balance;
 			const tx = await contract.withdraw(b.toString());
+
 			await tx.wait();
 			await refreshBalance();
 		}
 	};
-
-	const generateWallet = () => {
-		wallet = ethers.Wallet.createRandom();
-		console.log(wallet);
-	};
 </script>
 
 <main class="w-screen h-screen bg-zinc-200 flex items-center flex-col justify-center">
-	<button onclick={generateWallet} class="button">Generate wallet</button>
-	{#if wallet}
-		<div class="mt-4">
-			<p>Address: {wallet.address}</p>
-			<p>Public key: {wallet.publicKey}</p>
-			<p>Private key: {wallet.privateKey}</p>
-		</div>
-	{/if}
 	{#if contract}
-		<button class="button" onclick={refreshBalance}>refresh balance</button>
-		<button class="button" onclick={deposit}>deposit</button>
-		<button class="button" onclick={withdraw}>Withdraw</button>
-		{#await balance}
-			<p>Balance: Loading...</p>
-		{:then b}
-			<p>Balance: {b.toString()}</p>
-		{/await}
+		<div class="p-3 flex flex-col gap-4">
+			<form class="form section">
+				<label for="d-amount" class="label">Amount of eth (example: 0.001 eth)</label>
+				<input type="number" name="dEthAmount" id="d-amount" class="input" />
+				<button class="button" onclick={deposit}>Deposit</button>
+			</form>
+			<div class="divide"></div>
+
+			<form class="form section">
+				<label for="w-amount" class="label">Amount of eth (example: 0.001 eth)</label>
+				<input type="number" name="wEthEmount" id="w-amount" class="input" />
+				<button class="button" onclick={withdraw}>Withdraw</button>
+			</form>
+			<div class="divide"></div>
+			<div class="section">
+				<button class="button" onclick={refreshBalance}>Refresh balance</button>
+				{#await balance}
+					<p>Balance: Loading...</p>
+				{:then b}
+					<p>Balance: {b.toString()}</p>
+				{/await}
+			</div>
+		</div>
 	{/if}
 </main>
 
 <style lang="postcss">
+	.section {
+		@apply bg-slate-300 p-3 rounded;
+	}
+	.divide {
+		@apply w-full h-0.5 bg-stone-950;
+	}
+	.form {
+		@apply flex flex-col gap-2;
+	}
+	.input {
+		@apply p-1 bg-stone-100 rounded-md;
+	}
+	.label {
+		@apply text-xl font-semibold;
+	}
 	.button {
-		@apply py-1 bg-emerald-400 hover:opacity-80 rounded-md px-3;
+		@apply py-2 text-lg bg-stone-950 text-stone-100 w-fit font-semibold hover:opacity-80 rounded-md px-4;
 	}
 </style>
